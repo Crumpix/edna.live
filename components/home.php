cd06f69901fa55a7de3698978213c78bf746cade
adventurousgeek <geekspod@gmail.com>
2019-03-27 01:59:28 +0500

    Content Added!

--- components/home.php
+++ components/home.php
@@ -1,10 +1,12 @@
 <section class="mast-head">
     <div class="container">
         <div class="row">
-            <div class="col-md-12">
+            <div class="col-md-7">
                 <h1>Your DNA is your property</h1>
                 <p>Forge your own destiny and define your  DNA. Influence how history unfolds by sharing your DNA with our community.Your decision to share can help cure cancer and reshape your own genetic code.</p>
                 <p>Learn how your DNA strand is being shared around the world and take control of what belongs to you.</p>
+            </div>
+            <div class="col-md-5">
             </div>
         </div>
     </div>
@@ -26,40 +28,40 @@
 </section>
 
 <section class="what-we-do">
-    <div class="container">
+    <div class="container-fluid">
         <div class="row">
-            <div class="col-md-6">
+            <div class="col-md-6 own-your-dna">
                 Own Your DNA
             </div>
-            <div class="col-md-6">
+            <div class="col-md-6 right-description">
                 <h3>Ownership</h3>
                 <p>Be the creator and realize the truth that lives within you. No one can claim ownership of your destiny, your DNA. With our proprietary software, you can track your DNA in all scientific areas of researched it is used and claim rights.  Share one destiny with us. Your destiny.</p>
             </div>
         </div>
         <div class="row">
-            <div class="col-md-6">
+            <div class="col-md-6 left-description text-right">
                 <h3>DECENTRLIZED</h3>
                 <p>Once we load your in our blockchain system, doctors around the world have access to this system and can use the data as needed in their research for the awakening of all of humanity.</p>
             </div>
-            <div class="col-md-6">
+            <div class="col-md-6 earn-tokens">
                 Earn Tokens
             </div>
         </div>
         <div class="row">
-            <div class="col-md-6">
+            <div class="col-md-6 save-lives">
                 Save Lives
             </div>
-            <div class="col-md-6">
+            <div class="col-md-6 right-description">
                 <h3>GIVES BACK</h3>
                 <p>EDNA Members support efforts to halt illegal poaching of our precious wildlife as well as help fight trafficking in humans by automatically sending a portion of their profits to support these programs.</p>
             </div>
         </div>
         <div class="row">
-            <div class="col-md-6">
+            <div class="col-md-6 left-description text-right">
                 <h3>Modification</h3>
                 <p>After we compile thousands of strands, we can have a unified database to find the best strands to duplicate and share with the world. People can choose the healthiest strands and modify their DNA accordingly.</p>
             </div>
-            <div class="col-md-6">
+            <div class="col-md-6 advancing-dna">
                 Advancing DNA
             </div>
         </div>
@@ -69,10 +71,16 @@
 <section class="autonomous-community">
     <div class="container">
         <div class="row">
-            <h1>Decentralised Autonomous ComMunity</h1>
-            <p>The EDNA DAC will allow people to control their own genetic data and more importantly genetic futures. It’s rather challenging for governments and organizations to restrict and control information once it’s on the blockchain. That one’s important, but that’s all I’m saying for now. </p>
-            <p>Also important is the ability to secure your personal genetic information, and be in control of its monetization.</p>
-            <a href="#">Learn More</a>
+            <div class="col-md-12">
+                <h1 class="text-uppercase">Decentralised Autonomous Community</h1>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-md-6">
+                <p>The EDNA DAC will allow people to control their own genetic data and more importantly genetic futures. It’s rather challenging for governments and organizations to restrict and control information once it’s on the blockchain. That one’s important, but that’s all I’m saying for now. </p>
+                <p>Also important is the ability to secure your personal genetic information, and be in control of its monetization.</p>
+                <a href="#">Learn More</a>
+            </div>
         </div>
     </div>
 </section>
@@ -80,10 +88,15 @@
 <section class="power-science">
     <div class="container">
         <div class="row">
-            <h1>Power Your Science</h1>
-            <p>When you awaken yourself to the Om, you will understand energy, business, love, and all else in between on a whole new level. This is best explained as a vibration that sends sensations to all parts of your body, mind, and spirit. </p>
-            <p>If you want to find the secrets of the universe, think in terms of energy, frequency and vibration. </p>
-            <p>Know Yourself</p>
+            <div class="col-md-5">
+                <img src="assets/images/power-science.png">
+            </div>
+            <div class="col-md-7">
+                <h1>Power Your Science</h1>
+                <p>When you awaken yourself to the Om, you will understand energy, business, love, and all else in between on a whole new level. This is best explained as a vibration that sends sensations to all parts of your body, mind, and spirit. </p>
+                <p>If you want to find the secrets of the universe, think in terms of energy, frequency and vibration. </p>
+                <p>Know Yourself</p>
+            </div>
         </div>
     </div>
 </section>
