b634fa81195d1f250946181fdf06dadb0cbbe2e9
adventurousgeek <geekspod@gmail.com>
2019-04-21 23:19:33 +0500

    Const Page Added!

---
+++ components/edna-con.php
@@ -0,0 +1,65 @@
+<section class="constitution-graphic">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-8">
+                <h1>THE EDNA CONSTITUTION</h1>
+                <p>We the members of this deceantralized autonomous community, hereinafter known as EDNA, do band together in cooperation and do pledge ourselves to defend and uphold the following ethical principles:</p>
+            </div>
+        </div>
+    </div>
+</section>
+
+<section class="tenants-section">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                <h2>Tenants</h2>
+                <div class="tenant-descriptions">
+                    <div class="tenant-description">
+                        <h3>IDENTITY</h3>
+                        <p>A humans personal genetic data is their very own biological identity, and as such, it may not belong to another.</p>
+                    </div>
+                    <div class="tenant-description">
+                        <h3>Ownership</h3>
+                        <p>To support, condone or allow ownership of another humans identity, is to support, condone or allow a form of human enslavement. We utterly and unequivocally oppose this.</p>
+                    </div>
+                    <div class="tenant-description">
+                        <h3>Privacy</h3>
+                        <p>We hold the principles above as matters of ethics, and superior to moral law or laws of man. We pledge therefore to form no law that is cross-purpose with these principles or that weakens or dilutes them in any way.</p>
+                    </div>
+                    <div class="tenant-description">
+                        <h3>Free Market</h3>
+                        <p>We trust in a free market overall, and so too with respect to genetic information. We agree the monetary value of an individuals genetic data is best determined by a free market.</p>
+                    </div>
+                    <div class="tenant-description">
+                        <h3>VOTING</h3>
+                        <p>We agree the DAC shall initially be governed by the principle of “One Genome, One Vote” until such time that the free market can establish that a particular genome intrinsically carries more value to the DAC than another based on whatever monetization model(s) the DAC may elect.</p>
+                    </div>
+                    <div class="tenant-description">
+                        <h3>Monetization</h3>
+                        <p>As the individual genome is also individual property, DNA owners are free to lease this property or otherwise monetize this data as they see fit. However no lease, rental, agreement or other disclosure (intentional or inadvertent) may ever transfer rights of ownership.</p>
+                    </div>
+                    <div class="tenant-description">
+                        <h3>Ethics</h3>
+                        <p>We hold the principles above as matters of ethics, and superior to moral law or laws of man. We pledge therefore to form no law that is cross-purpose with these principles or that weakens or dilutes them in any way.</p>
+                    </div>
+                    <div class="tenant-description">
+                        <h3>Modification</h3>
+                        <p>We uphold and reserve the right in full to modify our genetic code however we deem advantageous to ourselves and by whatever means are available to us provided the principle of non-violence is respected and integral to the change. Just as one may not own another humans identity, no human or group of humans may dictate what another humans identity must be.</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+<section class="constitution-info">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                <p>Because we pledge ourselves to conduct our activities in support of these principles, we agree that should we discover our personal intentions to be at cross-purpose with these ethics, we shall recuse ourselves from all voting, and/or efforts to influence the DAC members until such time as we can re-align our personal intentions and purposes with those expressed above.</p>
+                <p> We acknowledge that The AGE Project, Inc. distributed the crypto-currency called EDNA Tokens at no cost to the recipients of these tokens, we further agree EDNA Tokens currently represent no claims of ownership, rights of control, voting powers or other matters of influence upon the DAC or EDNA itself. This agreement does not preclude the DAC from determining that its profits may be distributed by apportion of tokens held, or for any other legal use the DAC may elect for the token.</p>
+            </div>
+        </div>
+    </div>
+</section>
--- index.php
+++ index.php
@@ -1,7 +1,8 @@
 
 <?php require_once __DIR__ . '/common-components/header.php'; ?>
 <main class="site-main">
-	<?php require_once __DIR__ . '/components/home.php'; ?>
+	<?php // require_once __DIR__ . '/components/home.php'; ?>
+	<?php require_once __DIR__ . '/components/edna-con.php'; ?>
 </main>
 <?php require_once __DIR__ . '/common-components/footer.php'; ?>
 
